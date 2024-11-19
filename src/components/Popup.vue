982cf1f5d7048a1c22c6ebb92fb5561bde31f88c
NishaantKrSingh <nishaantkrsingh@gmail.com>
2024-11-20 02:06:40 +0530

    added functionality to take input of number and dob while also storing everything properly in chrome's storage to be accessed by the main page

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    <h3>Set New Tab Text</h3>
-    <input v-model="inputText" type="text" placeholder="Enter text" />
+    <h3>Continue timer till</h3>
+    <input v-model="inputText" type="number" min="1" max="100" placeholder="years" />
+
+  </div>
+    <div class="container">
+    <h1>Countdown to {{ inputText }} Years from DOB</h1>
+    <label for="dob">Enter your Date of Birth:</label>
+    <input type="date" v-model="dobInput">
     <button @click="saveText">Save</button>
+    <p>{{ dobInput }}</p>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-
 // Reactive variable for input
-const inputText = ref("");
+const inputText = ref('');
+const dobInput = ref('')
 
 // Save the input text to Chrome storage
 const saveText = () => {
-  chrome.storage.sync.set({ customText: inputText.value }, () => {
+  chrome.storage.sync.set({ customText: inputText.value, mainDOB: dobInput.value }, () => {
     console.log('Custom text saved:', inputText.value);
   });
 };
 </script>
 
-<style scoped>
-/* Add your styling here */
-</style>
