8c700c9aa82997d444d9d83c5b7e1658417c78a0
NishaantKrSingh <nishaantkrsingh@gmail.com>
2024-11-25 19:26:47 +0530

    component to show links to websites with their images

---
+++ src/components/comp/Shortcut.vue
@@ -0,0 +1,11 @@
+<template>
+    <a :href='props.link' target="_blank"><img :src="logo" width='50' height='50'></a>
+    <p>{{ props.name }}</p>
+</template>
+
+<script setup>
+const props = defineProps(['link', 'name'])
+const logo = `https://www.google.com/s2/favicons?sz=64&domain=${props.link}`
+console.log(logo);
+
+</script>
