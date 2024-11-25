92702b7ef28f836a88ac31f10c27060143195d86
NishaantKrSingh <nishaantkrsingh@gmail.com>
2024-11-25 21:07:15 +0530

    this component helps in adding profile and gmail links

---
+++ src/components/comp/Profile.vue
@@ -0,0 +1,11 @@
+<template>
+    <a :href="profile" target="_blank">Profile {{ props.usrNo }}</a> &emsp;
+    <a :href="gmail" target="_blank">Gmail {{ props.usrNo }}</a>
+    <br>
+</template>
+
+<script setup>
+const props = defineProps(['usrNo'])
+const profile = `https://www.google.com/webhp?authuser=${props.usrNo}`
+const gmail = `https://mail.google.com/mail/u/${props.usrNo}/?tab=rm&ogbl#inbox`
+</script>
